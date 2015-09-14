975158b12ef0fcf4c4d865978de3e60c8bbc1c6e
David Mohl <me@dave.cx>
2015-09-14 18:59:59 +0900

    added `q` keybinding to git status

--- layers/git/git.py
+++ layers/git/git.py
@@ -10,6 +10,7 @@ class Layer():
         { "keys": [" ", "g", "p"], "command": "git_push", "args": {}, "context": [{"key": "setting.command_mode"}] },
         { "keys": ["j"], "command": "git_status_move", "args": {"goto": "item:next"}, "context": [{ "key": "selector", "operator": "equal", "operand": "text.git-status" }] },
         { "keys": ["k"], "command": "git_status_move", "args": {"goto": "item:prev"}, "context": [{ "key": "selector", "operator": "equal", "operand": "text.git-status" }] },
+        { "keys": ["q"], "command": "close", "args": {}, "context": [{ "key": "selector", "operator": "equal", "operand": "text.git-status" }] },
     ]
 
     syntax_definitions = {}
