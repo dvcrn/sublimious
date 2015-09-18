8e30c4010f36fc9961b217067124031500d09f5c
David Mohl <me@dave.cx>
2015-09-18 11:54:25 +0900

    Shipping with (slightly adjusted) Solarized Dark now

--- layers/core/settings.py
+++ layers/core/settings.py
@@ -1,6 +1,6 @@
 {
     "theme": "Soda SolarizedDark.sublime-theme",
-    "color_scheme": "Packages/Color Scheme - Default/Solarized (Dark).tmTheme",
+    "color_scheme": "Packages/sublimious/Solarized Dark (sublimious).tmTheme",
 
     "font_face": "Hack",
     "font_size": 15,
