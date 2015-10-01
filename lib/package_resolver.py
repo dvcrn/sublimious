662a01515a92609fcb820dc0bff7c91655a9464f
David Mohl <me@dave.cx>
2015-10-01 10:47:35 +0900

    moved setting file writing into callback after packages got installed

--- lib/package_resolver.py
+++ lib/package_resolver.py
@@ -2,10 +2,11 @@ import threading
 
 
 class PackageResolver(threading.Thread):
-    def __init__(self, package_control, packages, to_remove):
+    def __init__(self, package_control, packages, to_remove, callback):
         self.package_control = package_control
         self.packages = packages
         self.to_remove = to_remove
+        self.callback = callback
         threading.Thread.__init__(self)
 
     def run(self):
@@ -14,3 +15,5 @@ class PackageResolver(threading.Thread):
 
         for pkg in self.packages:
             self.package_control.install_package(pkg)
+
+        self.callback()
