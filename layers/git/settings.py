f36eb38917a925c9a22164b2c9314c3611609cdc
David Mohl <me@dave.cx>
2015-10-06 17:40:11 +0900

    removed gitgutter from statusbar

--- layers/git/settings.py
+++ layers/git/settings.py
@@ -2,7 +2,11 @@
     "package_settings": {
         "GitSavvy": {
             "show_commit_diff": True,
-            "show_git_flow_commands": True
+            "show_git_flow_commands": True,
+            "git_status_in_status_bar": True,
+        },
+        "GitGutter": {
+            "show_status": "none",
         }
     }
 }
