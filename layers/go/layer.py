3896fff47a28fbdf697a89470d5304a4a59249b7
David Mohl <me@dave.cx>
2016-08-02 12:21:24 +0900

    feat(go): add simple go layer

---
+++ layers/go/layer.py
@@ -0,0 +1,16 @@
+class Layer():
+    name = "Go"
+
+    required_packages = [
+        "GoSublime"
+    ]
+
+    sublimious_keymap = []
+    sublime_keymap = []
+
+    syntax_definitions = {
+        "GoSublime-Go": ['go']
+    }
+
+    def init(self, config):
+        pass
